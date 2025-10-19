67d691a26232bf4b51d956035f8abdce3308999b
aru456 <saiyanamadala456@gmail.com>
2025-10-19 15:51:20 -0400

    prompt template added

---
+++ 2_PromptTemplate/1_prompt_template_basic.py
@@ -0,0 +1,26 @@
+from dotenv import load_dotenv
+from langchain_core.messages import HumanMessage
+from langchain.prompts import ChatPromptTemplate
+from langchain_openai import ChatOpenAI
+
+load_dotenv()
+
+template = "Tell me {number} jokes about {topic}."
+prompt_template = ChatPromptTemplate.from_template(template)
+
+
+def build_prompt(topic, num=2):
+    print("======Prompt from Template=======")
+    prompt = prompt_template.invoke({"topic": topic, "number" :num})
+    return prompt
+
+def call_model(prompt):
+    model = ChatOpenAI(model = "gpt-5-mini")
+    res = model.invoke(prompt)
+    print(res.content)
+
+topic = input("Enter an animal name : ")
+num = input("How many jokes ? ")
+prompt = build_prompt(topic,num)
+call_model(prompt)
+
